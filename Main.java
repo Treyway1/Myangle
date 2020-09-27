0cddbb8afa1f6a5a44415ef5bbf46de4ca6fb734
Treyway1 <treyvor.romero-jones@patelhs.org>
2020-09-27 00:50:52 +0000

    I did project mini 1.

--- Main.java
+++ Main.java
@@ -1,5 +1,10 @@
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+   int myAge = 15
+   int myBirthday = 3/21/2005
+   int todaysDate = 9/26/2020
+    System.out.println("I am" + myAge + "years old.");
+    System.out.println("My birthday is" + myBirthday + ".");
+    System.out.println("Today's date is" + todaysDate + ".");
   }
 }
