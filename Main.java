f4bcc59b993551977bcd6e3c0498c8650b3b06f8
Treyway1 <treyvor.romero-jones@patelhs.org>
2020-10-19 17:08:01 +0000

    I fixed the code.

--- Main.java
+++ Main.java
@@ -1,10 +1,10 @@
 class Main {
   public static void main(String[] args) {
-   int myAge = 15
-   int myBirthday = 3/21/2005
-   int todaysDate = 9/26/2020
+   int myAge = 15;
+   int myBirthday = (321);
+   int todaysDate = (910);
     System.out.println("I am" + myAge + "years old.");
-    System.out.println("My birthday is" + myBirthday + ".");
-    System.out.println("Today's date is" + todaysDate + ".");
+    System.out.println("My birthday is " + myBirthday + ".");
+    System.out.println("Today's date is " + todaysDate + ".");
   }
 }
